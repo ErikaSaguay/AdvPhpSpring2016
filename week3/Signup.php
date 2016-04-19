784e864aff6b9a6ff0774c1e5488ddba0a3e7c5f
ErikaSaguay <ecguaman@email.neit.edu>
2016-04-18 22:25:15 -0500

    Update Signup.php

--- week3/Signup.php
+++ week3/Signup.php
@@ -9,48 +9,34 @@
         include './autoload.php';
          $util = new Util();
          $log = new Functions();
+         $validator = new Validator();
+         $add = new Functions();
          $values = filter_input_array(INPUT_POST);
-        
-        if($util->setRequest){
-            $email=$pass="";
-            $dsn = 'mysql:host=localhost;dbname=phpadvclassspring2016';
-            $user='advphp';
-            $pass='php';
-            try{
-                $conn=new PDO($dsn,$user,$pass); // creating new php data object
-                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                
-            } 
-            catch (PDOException $e){
-                $error_message = $e->getMessage();//we use the rocket because it will be wrong to use a do
-                echo $error_message;
-                exit ();
-                
-            }
-            $created=date("Y-m-d H:i:s");
-            if(empty($_POST["email"])){
-                $emailErr="Email is required";
+         $email = $values['email'];
+         $pass = $values['password'];
+        $created=date("Y-m-d H:i:s");
+         $error = [];
+        if( $util->isPostRequest() ){
+
+            
+            if(empty($email){
+                $error['email'] = "Email is required";
             }
             if(empty($_POST["password"])){
-                $passErr="Please enter password";
+                $error['pass'] = "Please enter password";
             }
             else {
-                $emailtest=$_POST["email"];
-                if ( !preg_match($emailregex, $emailtest) ) {
-                $emailRegexErr = 'email is invalid';    
+                if ( !$validator->isEmailValid($values['email']) ) {
+                    $error['emailRegex'] = 'email is invalid';    
                 }else {
-                  
-                $email = $_POST["email"]; 
-                $passqord = $_POST["password"]; 
-                $sql="SELECT * FROM users WHERE email='".$email."'";
-                $result=  mysql_query($sql);
-                $query_data=  mysql_fetch_row($result);
-                if($query_data[0]> 0){
-                    echo"already exists";
+                    $add->unique($values);
+
+                if($add == true){
+                    $error['exists'] = "already exists";
                 }else{
-                    $sql2 = "INSERT INTO users(email,password,created)VALUES ('$email','$pass','$created')";// use exec() because no results are returned
-                    $conn->exec($sql2);
-                    echo"You have signed in";
+                    $add->create($values);
+                    $message['success'] = "Successfully added";
+
                 }
 
                 } 
@@ -61,14 +47,17 @@
         ?>
         <h3>Sign Up</h3>
         <form actio="#" method="post">
+            <span><?php echo $error['pass']; ?></span>
+            <span><?php echo $error['email']; ?></span>
+            <span><?php echo $error['emailRegex']; ?></span>
+            <span><?php echo $message['success']; ?></span>
             <p>Email:</p>
             <input type="text" name="email"><br>
             <p>Password:</p>
             <input type="password" name="password"><br><br>
             <input type="submit" name="submit" value="signup" >
         </form>
-        <p><?php echo $emailErr; ?></p>
-        <p><?php echo $passErr; ?></p>
+
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     </body>
     
